2d1c23c497e55c2c2446b63cf048afadc19daba0
Ben Lambert <veganhacker@gmail.com>
2015-01-19 20:52:40 -0500

    change the drush install method.

--- recipes/php.rb
+++ recipes/php.rb
@@ -3,16 +3,5 @@ package 'php-pear'
 package 'php5-fpm'
 package 'php5-gd'
 package 'php5-mysql'
-
-execute 'Update Pear' do
-  command "pear channel-discover pear.drush.org"
-  action :run
-  not_if 'pear list-all -c  pear.drush.org | grep drush/drush -c'
-end
-
-execute 'Install Drush' do
-  command "pear install drush/drush"
-  action :run
-  not_if 'pear list-all -c  pear.drush.org | grep drush/drush -c'
-end
+package 'drush'
   
